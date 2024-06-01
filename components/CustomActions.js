9ea61c8d59d9d16c2d3f3699ee90c49cd08050e7
Alexis Abner <alexisabner@alexiss-air.lan>
2024-05-31 18:55:33 -0600

    Add pickImage, takePhoto methods

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,15 +1,12 @@
 import  * as ImagePicker from 'expo-image-picker';
 import { Alert } from 'react-native';
 
-const CustomActions = ({ userID }) => {
+const CustomActions = ({ userID, onSend }) => {
 
     const generateReference = (uri) => {
         const timeStamp = (new Date()).getTime();
-        const imgName = uri.split('/')[]
-    }
-
-    const shareImage = async (imgURI) => {
-        //TO DO
+        // Image name is extracted from the URI
+        const imageName = imageUri.split('/').pop(); 
     }
 
     const pickImage = async () => {
@@ -22,12 +19,47 @@ const CustomActions = ({ userID }) => {
                 quality: 1,
             });
     
-            if (!result.canceled) shareImage(result.assets[0].uri);
+            if (!result.canceled) uploadImageToFirebase(result.assets[0].uri);
     
             else Alert.alert('Permission to access media library is required!');
         }
     };
-    
+
+    const takePhoto = async () => {
+        let permissions = await ImagePicker.requestCameraPermissionsAsync();
+
+        if (permissions?.granted) {
+            let result = await ImagePicker.launchCameraAsync();
+
+            if (!result.canceled) await uploadImageToFirebase(result.assets[0].uri);
+            else Alert.alert('You need to grant camera permissions to take a photo.');
+        };
+    }
+
+    const uploadImageToFirebase = async (imgURI) => {
+       try {
+            // Convert the image to a blob
+            const response = await fetch(imgURI);
+            const blob = await response.blob();
+
+            // Create a reference
+            const imgReference = generateReference(imgURI);
+
+            // Upload the file
+            const snapshot = await uploadBytes(imgReference, blob);
+
+            // Get the download URL
+            const downloadURL = await getDownloadURL(snapshot.ref);
+            onSend({ image: downloadURL })
+
+            // Display success message with download URL
+            console.log('Upload successful!');
+            console.log(`Image available at: ${downloadURL}`);
+        } catch (error) {
+            // Display error message if upload fails
+            Alert.alert('Upload failed.')
+        }
+    }    
 }
 
 export default CustomActions;
